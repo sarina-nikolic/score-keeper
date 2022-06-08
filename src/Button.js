0521b88985539b51c2367a0b1f1202b61ee98ce3
Sarina Nikolić <sarina.nikolic@gmail.com>
2022-06-08 14:11:58 +0200

    Add styled component

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,15 @@
-import "./Button.css";
+import styled from "styled-components";
 
 export default function Button({ onClick, children }) {
-  return (
-    <button onClick={onClick} className="Button">
-      {children}
-    </button>
-  );
+  return <button onClick={onClick}>{children}</button>;
 }
+
+const Button = styled.button`
+  padding: 1rem;
+  gap: 0.5rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  border-radius: 100px;
+}
+`;
